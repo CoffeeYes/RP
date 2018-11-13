07214c7d95b16ad573eb5787607f2ba2eb326d34
CoffeeYes <culinecro@gmail.com>
2018-11-13 15:45:00 +0100

    added basic markup and css to splash

--- src/views/splash.js
+++ src/views/splash.js
@@ -4,7 +4,18 @@ class Splash extends Component {
 
   render() {
     return (
-      <p>Splashscreen</p>
+      <div className="content-container">
+        <div className="login-container">
+          <form action="/login" method="POST" className="login-form">
+            <label>Username:</label>
+            <input name="username"/>
+            <label>Password:</label>
+            <input name="password"/>
+            <button>Login</button>
+            <a href="/enter-code">Got a Code?</a>
+          </form>
+        </div>
+      </div>
     );
   }
 }
