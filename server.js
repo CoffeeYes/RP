b558776cb2514bcb5afb5212bab6b31c40a112e4
CoffeeYes <culinecro@gmail.com>
2018-11-16 16:30:14 +0100

    set up bparser and login form to backend

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ const express = require('express');
 const bodyParser = require('body-parser')
 const path = require('path');
 const app = express();
+
+
 app.use(express.static(path.join(__dirname, 'build')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/ping', function (req, res) {
  return res.send('pong');
@@ -12,4 +16,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
+app.post('/login',function(req,res,next) {
+  console.log(req.body);
+})
 app.listen(process.env.PORT || 5000);
