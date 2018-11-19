42b6b72e731d012b6a8d03774132e472fb30eb64
CoffeeYes <culinecro@gmail.com>
2018-11-19 17:45:06 +0100

    added basic landing hero

--- src/views/splash.js
+++ src/views/splash.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import main_img from '../assets/main.jpg';
 
 class Splash extends Component {
 
   render() {
     return (
       <div className="content-container">
+        <img className="main-bg-img" src={main_img}/>
         <div className="login-container">
           <form action="/login" method="POST" className="login-form">
             <label>Username:</label>
