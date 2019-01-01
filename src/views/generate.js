20bedd7211b596bded4b4bc3b9ae3aa687741ee7
CoffeeYes <culinecro@gmail.com>
2019-01-01 11:54:02 +0100

    aligned lobby items correctly for admin login

--- src/views/generate.js
+++ src/views/generate.js
@@ -4,11 +4,9 @@ class Generate extends Component {
 
   render() {
     return (
-      <div className="horizontal-center">
         <form className="generateForm">
           <button onClick={this.props.createCode} className="generateButton">Generate Code</button>
         </form>
-      </div>
     );
   }
 }
