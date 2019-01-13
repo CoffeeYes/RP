217965ee9bf0a42ebe6f63caa8d18c71d8ef5155
CoffeeYes <culinecro@gmail.com>
2019-01-13 11:26:15 +0100

    css changes to userlist

--- src/views/userlist.js
+++ src/views/userlist.js
@@ -6,9 +6,19 @@ class UserList extends Component {
     return (
       <div className="horcent">
         <ul>
+        <div className="userListHead">
+          <p className="userListText">Display Name</p>
+          <p className="userListText">username</p>
+          <p className="userListText">Password</p>
+        </div>
         {this.props.list.map((item,index) => {
           return (
-            <li key={index}>{item.displayname} {item.username} {item.password}</li>
+            <div key={index} className="userListItem">
+              <p className="userListText">{item.displayname}</p>
+              <p className="userListText">{item.username}</p>
+              <p className="userListText">{item.password}</p>
+              <button name="deleteUser" className="deleteUserBtn">X</button>
+            </div>
           )
         })}
         </ul>
