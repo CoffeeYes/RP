fe7e87511af20292ed3f3522093d3d75e1e3812f
CoffeeYes <culinecro@gmail.com>
2019-03-11 16:03:11 +0100

    removed react-webcam, require custom implementation

--- src/views/koth.js
+++ src/views/koth.js
@@ -1,29 +1,37 @@
 import React, { Component } from 'react';
-import Webcam from 'react-webcam'
 
 class Koth extends Component {
+  setRef = webcam => {
+    this.webcam = webcam
+  }
 
+  handleUserMedia = () => {
+    console.log(this.webcam.stream)
+  }
+
+  record = () => {
+    setInterval( () => {
+      console.log("image")
+    },(30/1000))
+  }
   render() {
+
     return (
       <div className="cam-container">
         <div className="cam-row">
           <div className="camBox guest">
             <p>name1</p>
-            <Webcam className="cam2"/>
           </div>
           <div className="camBox guest">
             <p>name2</p>
-            <Webcam className="cam2"/>
           </div>
         </div>
         <div className="cam-row">
           <div className="camBox guest">
             <p>name3</p>
-            <Webcam className="cam2"/>
           </div>
           <div className="camBox guest">
             <p>name4</p>
-            <Webcam className="cam2"/>
           </div>
         </div>
       </div>
