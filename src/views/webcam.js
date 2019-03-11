0ea9e6c22b2b952e98056d3000e24848c9fa95cf
CoffeeYes <culinecro@gmail.com>
2019-03-11 16:40:34 +0100

    working video feed in webcam component

--- src/views/webcam.js
+++ src/views/webcam.js
@@ -1,30 +1,22 @@
 import React,{Component} from 'react';
 
 export default class Webcam extends Component {
-  constructor(props) {
-    super(props)
 
-    this.videoRef = React.createRef()
-    this.state = {
-      videoSrc : new MediaStream()
-    }
-  }
-
-  componentDidMount() {
+  componentDidMount = () => {
     let constraints = {
-      video : {width: 640,height : 480},
-      audio: true
+      video : {width: 640,height : 480}
     }
 
     navigator.mediaDevices.getUserMedia(constraints)
     .then( (stream) => {
-      this.setState({videoSrc : stream})
-      this.videoRef.current.srcObject = this.state.videoSrc;
+      this.setState({videoSrc : stream })
+      var video = document.querySelector('#testCam')
+      video.srcObject = stream
     })
   }
   render() {
     return(
-      <video autoplay ref={this.videoRef} className="cam2"></video>
+      <video autoPlay="true" className="cam2" id="testCam"></video>
     )
   }
 }
