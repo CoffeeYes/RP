49059ee8eae4072f2854bfddb67a3058af67d5a4
CoffeeYes <culinecro@gmail.com>
2019-04-18 12:43:11 +0200

    fixed vote percentage calculation and display

--- src/views/pollList.js
+++ src/views/pollList.js
@@ -10,23 +10,29 @@ class pollList extends Component {
     return (
       <div className="pollContainer">
       {this.props.pollsData.map((item,index) => {
+        //calculate total number of votes
         let sumVote = 0;
         for(var vote in Object.keys(item)) {
-          sumVote += Object.keys(item)[vote];
+          if(Object.keys(item)[vote] != 'code') {
+            sumVote += item[Object.keys(item)[vote]];
+          }
         }
         return (
-          <div className="pollItem">
+          <div className="pollItem" key={index}>
             {Object.keys(item).map((keyItem,keyIndex) => {
               if(keyItem != "code") {
                 return(
-                  <p className="pollChoice">{keyItem} : {item[keyItem]} ({(item[keyItem] == 0 ? 0 : item[keyItem] / sumVote)} %)</p>
+                  <p className="pollChoice" key={keyIndex}>{keyItem} : {item[keyItem]} ({(item[keyItem] == 0 ? 0 : (item[keyItem] / sumVote) * 100)} %)</p>
                 )
               }
               else {
                 return (
-                  <form className="deletePollForm" method="POST" action="/deletePoll">
-                    <button name="pollCode" value={item[keyItem]} onClick={this.props.deletePoll}>X</button>
-                  </form>
+                  <div key={keyIndex}>
+                    <p className="pollChoice">Code : {item[keyItem]}</p>
+                    <form className="deletePollForm" method="POST" action="/deletePoll">
+                      <button name="pollCode" value={item[keyItem]} onClick={this.props.deletePoll}>X</button>
+                    </form>
+                  </div>
                 )
               }
             })}
