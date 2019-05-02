e07ffb23b729415fa2d217db18cbfa05f76727ac
CoffeeYes <culinecro@gmail.com>
2019-05-02 11:55:39 +0200

    added button to toggle blur

--- src/views/cam.js
+++ src/views/cam.js
@@ -3,11 +3,25 @@ import main_img from '../assets/main.jpg';
 
 class Cam extends Component {
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      camFilter : "camBlurred"
+    }
+
+    this.toggleFilter = this.toggleFilter.bind(this);
+  }
+
+  toggleFilter(event) {
+    this.state.camFilter == "camBlurred" ? this.setState({camFilter : "camNotBlurred"}) : this.setState({camFilter : "camBlurred"})
+  }
   render() {
     return (
       <div>
         <p>{this.props.camName}</p>
-        <video autoPlay={true} className={["cam " + this.props.camType]} id={this.props.camID}></video>
+        <video autoPlay={true} className={["cam " + this.props.camType + " " + this.state.camFilter]} id={this.props.camID}></video>
+        <button onClick={this.toggleFilter}> B </button>
       </div>
     );
   }
