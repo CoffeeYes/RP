f361d7a9cec0041be3e52de73e9c50a50b0e46fa
CoffeeYes <culinecro@gmail.com>
2019-06-16 12:57:51 +0200

    added yes/no buttons for host, seperate render for anons

--- src/views/koth.js
+++ src/views/koth.js
@@ -99,7 +99,7 @@ class Koth extends Component {
         </div>
       );
     }
-    else {
+    else if(this.props.userType == "host") {
       return (
           <div className="cam-container">
             <div className="cam-col">
@@ -110,12 +110,35 @@ class Koth extends Component {
             <div className="cam-col">
                 <Cam camName="Contestant" camID="contestant1" camType="contestantCam" userType={this.props.userType} containerType="remoteCam contestant" audioID="audioContestant1"/>
                 <Cam camName="King" camID="contestant2" camType="contestantCam" userType={this.props.userType} containerType="remoteCam contestant" audioID="audioContestant2"/>
+                <div className="buttonsContainer">
+                  <button>Yes</button>
+                  <button>No</button>
+                </div>
             </div>
             <div className="cam-col">
                 <Cam camName={this.state.name2} camID="remote2" camType="guestCam" userType={this.props.userType} containerType="remoteCam guest" audioID="audioGuest2" />
                 <Cam camName={this.state.name3} camID="remote3" camType="guestCam" userType={this.props.userType} containerType="remoteCam guest" audioID="audioGuest3" />
             </div>
           </div>
+      )
+    }
+    else {
+      return (
+        <div className="cam-container">
+          <div className="cam-col">
+              <Cam camName={this.props.localUsername} camID="localCam" camType="guestCam" userType={this.props.userType} containerType="localCam guest"/>
+              <Webcam userType={this.props.userType} localUsername={this.props.localUsername} updateUsername={(number,name) => this.updateUsername(number,name)}/>
+              <Cam camName={this.state.name1} camID="remote1" camType="guestCam" userType={this.props.userType} containerType="remoteCam guest" audioID="audioGuest1"/>
+          </div>
+          <div className="cam-col">
+              <Cam camName="Contestant" camID="contestant1" camType="contestantCam" userType={this.props.userType} containerType="remoteCam contestant" audioID="audioContestant1"/>
+              <Cam camName="King" camID="contestant2" camType="contestantCam" userType={this.props.userType} containerType="remoteCam contestant" audioID="audioContestant2"/>
+          </div>
+          <div className="cam-col">
+              <Cam camName={this.state.name2} camID="remote2" camType="guestCam" userType={this.props.userType} containerType="remoteCam guest" audioID="audioGuest2" />
+              <Cam camName={this.state.name3} camID="remote3" camType="guestCam" userType={this.props.userType} containerType="remoteCam guest" audioID="audioGuest3" />
+          </div>
+        </div>
       )
     }
   }
