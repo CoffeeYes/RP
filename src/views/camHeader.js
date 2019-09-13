2f9e1de50eb449005b735a32048cd097b3d5c8b6
CoffeeYes <culinecro@gmail.com>
2019-09-13 16:17:06 +0200

    fixed camheader button render for different usertypes

--- src/views/camHeader.js
+++ src/views/camHeader.js
@@ -47,13 +47,6 @@ class camHeader extends Component {
               <div className="buttonContainer">
                 <img src={this.props.tickIcon} id={["tick" + this.props.iconID]} alt="tick icon"/>
                 <img src={this.props.crossIcon} id={["cross" + this.props.iconID]} alt="cross icon"/>
-                <button className="cameraButton" onClick={this.props.toggleFilter}>
-                  <img src={this.props.videoIcon} alt="video icon"/>
-                </button>
-                <button className="cameraButton" onClick={this.props.toggleMute}>
-                  <img src={this.props.muteIcon} alt="mute icon"/>
-                </button>
-                <button className="cameraButton" onClick={() => this.props.kickUser(this.props.camID)}>Kick</button>
               </div>
           </div>
         )
@@ -62,15 +55,6 @@ class camHeader extends Component {
         return(
           <div className="camHeader">
               <p className="usersName">{this.props.camName}</p>
-              <div className="buttonContainer">
-                <button className="cameraButton" onClick={this.props.toggleFilter}>
-                  <img src={this.props.videoIcon} alt="video icon"/>
-                </button>
-                <button className="cameraButton" onClick={this.props.toggleMute}>
-                  <img src={this.props.muteIcon} alt="mute icon"/>
-                </button>
-                <button className="cameraButton" onClick={() => this.props.kickUser(this.props.camID)}>Kick</button>
-              </div>
           </div>
         )
       }
