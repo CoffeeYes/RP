a7f0c935a3a82de2d0d959e5099917cf0ec4d9ed
CoffeeYes <culinecro@gmail.com>
2019-10-22 14:56:27 +0200

    created user/admin renders for bachelor component

--- src/views/bachelor.js
+++ src/views/bachelor.js
@@ -36,47 +36,85 @@ class Bachelor extends Component {
   }
 
   render() {
-    return (
-      <div>
-        <div className="bachelor-main">
-          <Webcam
-          socket={this.props.socket}
-          userType={this.props.userType}
-          localUsername={this.props.localUsername}
-          kickUserFromLobby={this.kickUserFromLobby}
-          personalPosition={this.state.personalPosition}
-          updateUsername={this.updateUsername}
-          kickUserFromLobby={this.props.kickUserFromLobby}
+    if(this.props.userType == "admin") {
+      return (
+        <div>
+          <div className="bachelor-main">
+            <Webcam
+            socket={this.props.socket}
+            userType={this.props.userType}
+            localUsername={this.props.localUsername}
+            kickUserFromLobby={this.kickUserFromLobby}
+            personalPosition={this.state.personalPosition}
+            updateUsername={this.updateUsername}
+            kickUserFromLobby={this.props.kickUserFromLobby}
+            />
+            <div className="cams-container">
+            {this.state.users.map( (item,index) => {
+              return(
+                <Cam
+                camID={["cam"] + (item.position)}
+                userType={this.props.userType}
+                tickIcon={"//:0"}
+                crossIcon={"//:0"}
+                camName={item.username}
+                camType="bachelorCam"
+                containerType="bachelorCamBox"
+                key={index}
+                allMuted={this.props.allMuted}
+                allBlurred={this.props.allBlurred}
+                kickUser={(camID) => this.props.kickUser(camID)}
+                />
+              )
+            })}
+            </div>
+          </div>
+          <AdminButtons
+          muteAll={this.props.muteAll}
+          unmuteAll={this.props.unmuteAll}
+          blurAll={this.props.blurAll}
+          unblurAll={this.props.unblurAll}
+          mode={this.props.mode}
           />
-          <div className="cams-container">
-          {this.state.users.map( (item,index) => {
-            return(
-              <Cam
-              camID={["cam"] + (item.position)}
-              userType={this.props.userType}
-              tickIcon={"//:0"}
-              crossIcon={"//:0"}
-              camName={item.username}
-              camType="bachelorCam"
-              containerType="bachelorCamBox"
-              key={index}
-              allMuted={this.props.allMuted}
-              allBlurred={this.props.allBlurred}
-              kickUser={(camID) => this.props.kickUser(camID)}
-              />
-            )
-          })}
+        </div>
+      );
+    }
+    else {
+      return (
+        <div>
+          <div className="bachelor-main">
+            <Webcam
+            socket={this.props.socket}
+            userType={this.props.userType}
+            localUsername={this.props.localUsername}
+            kickUserFromLobby={this.kickUserFromLobby}
+            personalPosition={this.state.personalPosition}
+            updateUsername={this.updateUsername}
+            kickUserFromLobby={this.props.kickUserFromLobby}
+            />
+            <div className="cams-container">
+            {this.state.users.map( (item,index) => {
+              return(
+                <Cam
+                camID={["cam"] + (item.position)}
+                userType={this.props.userType}
+                tickIcon={"//:0"}
+                crossIcon={"//:0"}
+                camName={item.username}
+                camType="bachelorCam"
+                containerType="bachelorCamBox"
+                key={index}
+                allMuted={this.props.allMuted}
+                allBlurred={this.props.allBlurred}
+                kickUser={(camID) => this.props.kickUser(camID)}
+                />
+              )
+            })}
+            </div>
           </div>
         </div>
-        <AdminButtons
-        muteAll={this.props.muteAll}
-        unmuteAll={this.props.unmuteAll}
-        blurAll={this.props.blurAll}
-        unblurAll={this.props.unblurAll}
-        mode={this.props.mode}
-        />
-      </div>
-    );
+      );
+    }
   }
 }
 
