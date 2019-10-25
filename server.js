27aca6bdf55fbd5f85ea9f7bb6a6e07bd5e4b6e4
CoffeeYes <culinecro@gmail.com>
2019-10-25 15:53:49 +0200

    mount existing bachelor cams for admin on login

--- server.js
+++ server.js
@@ -353,9 +353,10 @@ io.on('connection',(client) => {
       bachelorUserList.push({position : bachelorPositionList[0],socketID : client.id,username : username})
       //remove position that was just assigned from position array
       bachelorPositionList.splice(0,1)
-      //send users and array to frontend for render
-      io.emit("receiveBachelorUserList",bachelorUserList)
+
     }
+    //send users and array to frontend for render
+    io.emit("receiveBachelorUserList",bachelorUserList)
   })
 })
 
