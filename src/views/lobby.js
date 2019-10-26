88fbb54f706a0137916dded27d3924d896702044
CoffeeYes <culinecro@gmail.com>
2019-10-26 11:34:18 +0200

    removed generate button from bachelor render

--- src/views/lobby.js
+++ src/views/lobby.js
@@ -11,16 +11,29 @@ class Lobby extends Component {
   }
 
   render() {
-    if(this.props.renderFull) {
-      return (
-        <div>
-          <div className="hor-center">
-            <Generate createCode={this.props.createCode} generatedCode={this.props.generatedCode} showCopied={this.props.showCopied}/>
-            <a href="/panel" className="panel-link">Panel</a>
+    if(this.props.userType == "admin") {
+      if(this.props.mode == "king of the hill") {
+        return (
+          <div>
+            <div className="hor-center">
+              <Generate createCode={this.props.createCode} generatedCode={this.props.generatedCode} showCopied={this.props.showCopied}/>
+              <a href="/panel" className="panel-link">Panel</a>
+            </div>
+            <RenderMode mode={this.props.mode} userType={this.props.userType} localUsername={this.props.localUsername} kickUserFromLobby={this.props.kickUserFromLobby}/>
           </div>
-          <RenderMode mode={this.props.mode} userType={this.props.userType} localUsername={this.props.localUsername} kickUserFromLobby={this.props.kickUserFromLobby}/>
-        </div>
-      )
+        )
+      }
+      else {
+        return (
+          <div>
+            <div className="hor-center">
+              <a href="/panel" className="panel-link">Panel</a>
+            </div>
+            <RenderMode mode={this.props.mode} userType={this.props.userType} localUsername={this.props.localUsername} kickUserFromLobby={this.props.kickUserFromLobby}/>
+          </div>
+        )
+      }
+
     }
     else {
       return (
