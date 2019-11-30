6ca66bb4a4d894c167c97204644578f263faeef8
CoffeeYes <culinecro@gmail.com>
2019-11-30 13:39:02 +0100

    increases add user form font size

--- src/views/addUser.js
+++ src/views/addUser.js
@@ -7,11 +7,11 @@ class addUser extends Component {
       <div className="horcent">
         <form className="addUserForm">
           <p className="error">{this.props.error}</p>
-          <label>Username</label>
+          <label className="addUserLabel">Username</label>
           <input name="username" className="addUserInput" onChange={this.props.update}/>
-          <label>Password</label>
+          <label className="addUserLabel">Password</label>
           <input name="password" className="addUserInput" onChange={this.props.update}/>
-          <label>Display Name</label>
+          <label className="addUserLabel">Display Name</label>
           <input name="displayname" className="addUserInput" onChange={this.props.update}/>
           <button onClick={this.props.addUser} id="addUserBtn">Add</button>
         </form>
