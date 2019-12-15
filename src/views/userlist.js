28c06b429964ca7730be6d14acb90489c2666f51
CoffeeYes <culinecro@gmail.com>
2019-12-15 13:12:36 +0100

    made userlist scrollable to stop overflow from hiding

--- src/views/userlist.js
+++ src/views/userlist.js
@@ -7,21 +7,23 @@ class UserList extends Component {
   }
   render() {
     return (
-      <div className="horcent userListContainer">
-        <ul>
-        {this.props.list.map((item,index) => {
-          return (
-            <form key={index} className="userListItem" method="post" action="/deleteUser">
-              <input className="userListText" value={item.displayname} name="displayname"/>
-              <div className="overlayBlock">
-                <input className="userListText" value={"username: " + item.username} name="username"/>
-                <input className="userListText" value={"password: " + item.clearTextPassword} name="password"/>
-              </div>
-              <button type="submit" className="deleteUserBtn" onClick={this.props.deleteUser} value={item.username}>X</button>
-            </form>
-          )
-        })}
-        </ul>
+      <div className="horcent">
+        <div className="horcent userListContainer">
+          <ul>
+          {this.props.list.map((item,index) => {
+            return (
+              <form key={index} className="userListItem" method="post" action="/deleteUser">
+                <input className="userListText" value={item.displayname} name="displayname"/>
+                <div className="overlayBlock">
+                  <input className="userListText" value={"username: " + item.username} name="username"/>
+                  <input className="userListText" value={"password: " + item.clearTextPassword} name="password"/>
+                </div>
+                <button type="submit" className="deleteUserBtn" onClick={this.props.deleteUser} value={item.username}>X</button>
+              </form>
+            )
+          })}
+          </ul>
+        </div>
       </div>
     );
   }
