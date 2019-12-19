84ecde8a5a9a0a52ca353989d1b19b8f447979ea
CoffeeYes <culinecro@gmail.com>
2019-12-19 15:08:22 +0100

    font size change on mode choice, made whole panel box a link

--- src/views/lobby.js
+++ src/views/lobby.js
@@ -17,9 +17,9 @@ class Lobby extends Component {
           <div>
             <div className="hor-center">
               <Generate createCode={this.props.createCode} generatedCode={this.props.generatedCode} showCopied={this.props.showCopied}/>
-              <div className="panel-link">
-                <a href="/panel">Panel</a>
-              </div>
+              <a href="/panel" className="panel-link">
+                  <p>Panel</p>
+              </a>
             </div>
             <RenderMode mode={this.props.mode} userType={this.props.userType} localUsername={this.props.localUsername} kickUserFromLobby={this.props.kickUserFromLobby}/>
           </div>
