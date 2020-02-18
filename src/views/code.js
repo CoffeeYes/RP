c22304d0ddb6683570f59ba806da5fc2f370d874
CoffeeYes <culinecro@gmail.com>
2020-02-18 15:46:47 +0100

    added background and changed button styling on code page

--- src/views/code.js
+++ src/views/code.js
@@ -4,12 +4,12 @@ class Code extends Component {
 
   render() {
     return (
-      <div className="content-container">
+      <div className="content-container-main">
         <form className="codeForm">
           <p className="error">{this.props.error}</p>
           <h2>Enter your code</h2>
           <input name="codeInput" onChange={this.props.handleTextChange}/>
-          <button onClick={this.props.handleCode}>Submit</button>
+          <button onClick={this.props.handleCode} className="blackButton" id="codeSubmitButton">Submit</button>
         </form>
       </div>
     );
