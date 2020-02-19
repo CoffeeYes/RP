86fb44e92d6786b263fb60c2a88ec0f9d34f12ba
CoffeeYes <culinecro@gmail.com>
2020-02-19 16:48:11 +0100

    added frosted glass to code page

--- src/views/code.js
+++ src/views/code.js
@@ -5,12 +5,14 @@ class Code extends Component {
   render() {
     return (
       <div className="content-container-main">
-        <form className="codeForm">
-          <p className="error">{this.props.error}</p>
-          <h2>Enter your code</h2>
-          <input name="codeInput" onChange={this.props.handleTextChange}/>
-          <button onClick={this.props.handleCode} className="blackButton" id="codeSubmitButton">Submit</button>
-        </form>
+        <div className="codeFormContainer">
+          <form className="codeForm">
+            <p className="error">{this.props.error}</p>
+            <h2>Enter your code</h2>
+            <input name="codeInput" onChange={this.props.handleTextChange}/>
+            <button onClick={this.props.handleCode} className="blackButton" id="codeSubmitButton">Submit</button>
+          </form>
+        </div>
       </div>
     );
   }
