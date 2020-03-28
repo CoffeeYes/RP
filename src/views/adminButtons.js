beb39d82bd7f48f647977f203d388e1b27444def
CoffeeYes <culinecro@gmail.com>
2020-03-28 14:38:26 +0100

    custom styling for koth admin buttons

--- src/views/adminButtons.js
+++ src/views/adminButtons.js
@@ -24,12 +24,12 @@ class AdminButtons extends Component {
     if(this.props.mode == "king of the hill") {
       return (
         <div className="buttonsContainer-koth">
-          <button onClick={this.props.swapContestants} className="adminButton blackButton">Swap</button>
-          <button onClick={this.props.muteAll} className="adminButton blackButton">Mute All</button>
-          <button onClick={this.props.unmuteAll} className="adminButton blackButton">Unmute All</button>
-          <button onClick={this.props.blurAll} className="adminButton blackButton">Blur All</button>
-          <button onClick={this.props.unblurAll} className="adminButton blackButton">Unblur All</button>
-          <button onClick={this.props.resetAll} className="adminButton blackButton">Reset Votes</button>
+          <button onClick={this.props.swapContestants} className="adminButton blackButton kothButton">Swap</button>
+          <button onClick={this.props.muteAll} className="adminButton blackButton kothButton">Mute All</button>
+          <button onClick={this.props.unmuteAll} className="adminButton blackButton kothButton">Unmute All</button>
+          <button onClick={this.props.blurAll} className="adminButton blackButton kothButton">Blur All</button>
+          <button onClick={this.props.unblurAll} className="adminButton blackButton kothButton">Unblur All</button>
+          <button onClick={this.props.resetAll} className="adminButton blackButton kothButton">Reset Votes</button>
         </div>
       );
     }
@@ -38,10 +38,10 @@ class AdminButtons extends Component {
         <div className="buttonAndHideContainer">
           <button className="hideButtonContainer blackButton" onClick={this.toggleHideButtonContainer}>{this.state.hideButtontext}</button>
           <div className={["buttonsContainer-bachelor " + this.state.buttonContainerClasses]} onClick={this.hideAdminButtons}>
-              <button onClick={this.props.muteAll} className="adminButton blackButton">Mute All</button>
-              <button onClick={this.props.unmuteAll} className="adminButton blackButton">Unmute All</button>
-              <button onClick={this.props.blurAll} className="adminButton blackButton">Blur All</button>
-              <button onClick={this.props.unblurAll} className="adminButton blackButton">Unblur All</button>
+              <button onClick={this.props.muteAll} className="adminButton blackButton bachelorButton">Mute All</button>
+              <button onClick={this.props.unmuteAll} className="adminButton blackButton bachelorButton">Unmute All</button>
+              <button onClick={this.props.blurAll} className="adminButton blackButton bachelorButton">Blur All</button>
+              <button onClick={this.props.unblurAll} className="adminButton blackButton bachelorButton">Unblur All</button>
           </div>
         </div>
       );
